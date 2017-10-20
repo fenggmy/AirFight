6c0d4608d0ce5f9c63de970c1db2a947819e5643
马异峰 <fengfeng@bogon>
2017-10-20 18:48:29 +0800

    初始化以及资源的添加

---
+++ AirFight/AirFight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirFight
+//
+//  Created by 马异峰 on 2017/10/20.
+//  Copyright © 2017年 Yifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AirFight/AirFight/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AirFight
+//
+//  Created by 马异峰 on 2017/10/20.
+//  Copyright © 2017年 Yifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
